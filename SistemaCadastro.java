9bffc0086222097b03563f37710ddf6e11c82adb
MailsonADS <129121481+MailsonADS@users.noreply.github.com>
2023-04-10 22:12:02 -0300

    Add files via upload

---
+++ SistemaCadastro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacadastro;
+
+/**
+ *
+ * @author 55499
+ */
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+        System.out.println("SITEMA INICIALIZOU...");
+        CalculoMedia guiP = new CalculoMedia();
+        CadastroAluno guiS = new CadastroAluno();
+       
+    }
+}
